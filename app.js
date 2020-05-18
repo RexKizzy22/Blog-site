de7542562f4adca93bc8b1d24f08998669359353
Kizito <kizitoinegbenose@gmail.com>
2020-05-18 21:40:43 +0100

    Add gitignore

--- app.js
+++ app.js
@@ -81,9 +81,9 @@ app.post("/compose", (req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log("Server started on port 3000");
 });
