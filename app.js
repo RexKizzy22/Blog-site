1ba96f287ba066f13a5ce9b727d53a0058496f60
Kizito <kizitoinegbenose@gmail.com>
2020-05-19 07:44:57 +0100

    Host database on MongoDB Atlas

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-kizito:Test123@cluster0-mg6qp.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const homeSchema = new mongoose.Schema({
   title: String,
